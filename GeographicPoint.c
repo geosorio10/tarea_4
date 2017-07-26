48b34471deb813a5bcaf80e01f87984f906fac84
German <ge.osorio10@uniandes.edu.co>
2017-07-26 18:05:43 -0500

    más o menos va el esqueleto

--- GeographicPoint.c
+++ GeographicPoint.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <time.h>
+
 #define DOUBLE float
 
 
@@ -10,6 +12,8 @@ int Nx;
 
 double **Matrix(void);
 void freePointer(double **matrix);
+int aleatorio();
+double radioMax(int x_0,int y_0,double **matrix);
 
 
 int main(void){
@@ -17,11 +21,12 @@ int main(void){
   FILE *file;
   file=fopen("map_data.txt","r");
 
-  int len=1500;
+  int len=2000;
   char line_buffer[len];
   char *split_buffer;
   const char *delimiter;
-  Nx=1500;
+
+  Nx=500;//por que sale segmentation fault si pongo 744X500
   Ny=1500;
 
   double **matrix=Matrix();
@@ -38,18 +43,39 @@ int main(void){
 
       while(split_buffer!=NULL)
 	{
-	  //printf("ITEM IN LINE: %s\n",split_buffer);
+	 
 	  //printf("ITEM IN LINE: %s %d %d\n",split_buffer,i,j);
 	  l=atof(split_buffer);
 	  matrix[i][j]=l;
-	  printf("%f\n",matrix[i][j]);
+	  
+	  //printf("%f\n",matrix[i][j]);
+	  
 	  split_buffer=strtok(NULL,delimiter);
 	  j+=1;
 	  
 	}
-      
+     
       i+=1;   
     }
+
+  int y_0=aleatorio()%1500;
+  int x_0=aleatorio()%500;
+
+  printf("%d %d\n",y_0,x_0);
+  printf("%f\n",matrix[y_0][x_0]);
+  
+  if(matrix[y_0][x_0]==0.0)
+    {
+     double r=radioMax( x_0, y_0, matrix);
+     
+     printf("%f\n",r);
+    }
+
+
+  
+  /* 
+  freePointer(matrix);
+ */
   
   return 0;
 }
@@ -69,7 +95,9 @@ double **Matrix(void)
     return matrix;
 }
 
+
 void freePointer(double **matrix)
+ 
 {
     int i;
     for(i = 0; i<Ny; i++)
@@ -78,3 +106,36 @@ void freePointer(double **matrix)
     }
     free(matrix);
 }
+
+int aleatorio()
+{
+  srand(time(NULL));
+
+  int  x=rand();
+  return x;
+  
+}
+
+
+double radioMax(int x_0,int y_0,double **matrix)
+{
+  
+  double r;
+  
+  int i;
+  
+  i=0;
+  
+  while( (matrix[y_0][x_0+i]==0.0) && (matrix[y_0+i][x_0]==0.0) && (matrix[y_0-i][x_0]==0.0) && (matrix[y_0][x_0-i]==0.0) )//me gustaria para que tome tambien un circulo de unos.
+    {
+      r=i;
+      
+      i+=1;
+    }
+
+  return r; 
+}
+
+
+
+
